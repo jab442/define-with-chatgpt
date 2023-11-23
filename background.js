3a0b34983f6b5c3be726610ecdbd1a367a3eac41
John Doe <johndoe@example.com>
2023-11-23 15:25:04 +1300

    Send selected text to api

--- background.js
+++ background.js
@@ -1,36 +1,51 @@
 import { ENV } from './define-with-chat-gpt-key.js';
 
 const apiKey = ENV.API_KEY;
-console.log("background");
+console.log("Background script running");
+
+// Listener for context menu selection
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "sendSelectedText",
+    title: "Send Selected Text",
+    contexts: ["selection"]
+  });
+});
+
+// Listener for context menu click
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "sendSelectedText") {
+    chrome.tabs.sendMessage(tab.id, { command: 'sendText' });
+  }
+});
+
+// Listener for messages from content scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("request"+request.text);
-    if (request.text) {
-      //fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+  console.log("Received request: ", request.text);
+  if (request.text) {
     fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization':  `Bearer ${apiKey}`// Be cautious with your API key; don't expose it in client-side scripts.
-        },
-        body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          //prompt: request.text,
-          //max_tokens: 150
-          messages:[{role:"user",content:request.text}],
-          temperature:0.5,
-        })
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}` // Be cautious with your API key
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: request.text }],
+        temperature: 0.5,
       })
-      .then(response => response.json())
-      .then(data => {
-        console.log("Data: " + data + " choices" + data.choices[0].message.content);
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("API Response: ", data);
+      if (data.choices && data.choices.length > 0) {
         chrome.tabs.sendMessage(sender.tab.id, { result: data.choices[0].message.content });
-        //alert("Data: " + data)
-        
-      })
-      .catch(error => {
-        console.error('Error:', error)
-        //alert("Error: " + error )
-      });
-    }
-  });
-  
+      } else {
+        console.error("No data in API response");
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+});
--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 // content.js
+/*
 function getSelectedText() {
     //let selectedText = window.getSelection().toString().trim();
     let selectedText="Say this: Hello, how are you?"
@@ -6,4 +7,14 @@ function getSelectedText() {
   }
   
   chrome.runtime.sendMessage({ text: getSelectedText() });
-  
+  */
+function getSelectedText() {
+    return window.getSelection().toString().trim();
+}
+
+// Listen for the context menu item click
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.command === 'sendText') {
+        chrome.runtime.sendMessage({ text: getSelectedText() });
+    }
+});
