2d22730e1ada9e9062e2e9fd4daddc320f1cd378
John Doe <johndoe@example.com>
2023-11-25 19:54:06 +1300

    Added context and changed model

--- background.js
+++ background.js
@@ -1,57 +1,67 @@
-//background.js
 import { ENV } from './define-with-chat-gpt-key.js';
 
 const apiKey = ENV.API_KEY;
 console.log("Background script running");
 let lastResponse = '';
+let model = "gpt-4-1106-preview";
+
 // Listener for context menu selection
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "sendSelectedText",
-        title: "Send Selected Text",
-        contexts: ["selection"]
-    });
+  chrome.contextMenus.create({
+    id: "sendSelectedText",
+    title: "Define with Chat GPT",
+    contexts: ["selection"]
+  });
 });
 
 // Listener for context menu click
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "sendSelectedText") {
-      let selectedText = info.selectionText;
-      if (selectedText) {
+  if (info.menuItemId === "sendSelectedText") {
+    let selectedText = info.selectionText;
+    if (selectedText) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => document.body ? document.body.innerText : '',
+      }, (injectionResults) => {
+        if (chrome.runtime.lastError || !injectionResults || injectionResults.length === 0 || !injectionResults[0].result) {
+          console.error('Error in script execution:', chrome.runtime.lastError?.message || 'No results returned');
+          return;
+        }
+
+        let pageText = injectionResults[0].result;
+        let content = `define: ${selectedText} in the context of: ${pageText}`;
+        let tokenCount = Math.ceil(content.length / 4);
+        content = (tokenCount <= 128000) ? content : `define: ${selectedText}`;
+
         fetch('https://api.openai.com/v1/chat/completions', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}` // Be cautious with your API key
+            'Authorization': `Bearer ${apiKey}`
           },
           body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: `define: ${selectedText}` }],
+            model: model,
+            messages: [{ role: "user", content: content }],
             temperature: 0.5,
           })
         })
         .then(response => response.json())
         .then(data => {
-          lastResponse = data.choices[0].message.content; // Store the response
-          //chrome.tabs.sendMessage(tab.id, { result: lastResponse });
-          // Set the data in storage
-            chrome.storage.local.set({ responseData: lastResponse }, function() {
-                console.log('Data is set to ' + lastResponse);
-                chrome.windows.create({
-                    url:  chrome.runtime.getURL("popup.html"),
-                    type: 'popup',
-                    width: 400,
-                    height: 300
-                });
+          lastResponse = data.choices[0].message.content;
+          chrome.storage.local.set({ responseData: lastResponse }, function () {
+            console.log('Data is set to ' + lastResponse);
+            chrome.windows.create({
+              url: chrome.runtime.getURL("popup.html"),
+              type: 'popup',
+              width: 400,
+              height: 300
             });
-            
-            // Create the popup window
-
+          });
         })
         .catch(error => {
           console.error('Error:', error);
         });
-      }
+      });
     }
-  });
-  
+  }
+});
